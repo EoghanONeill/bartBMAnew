614e669894db6082d32bc2e950242244724bf1d2
Eoghan O'Neill <oneill.eoghan.1@gmail.com>
2019-01-09 15:47:01 +0000

    Added a function pred_means_bbma.R for obtaining predictions as the means from a Gibbs sampler [only needed to change pred_intervals function so that apply mean() rather than quantile()]. Also added these means to the pred_intervals.R output.

--- R/pred_means.R
+++ R/pred_means.R
@@ -1,18 +1,14 @@
-#' @title Prediction intervals for bart-bma output
+#' @title Predictions for bart-bma output obtained from a Gibbs sampler
 #' 
-#' @description This function produces prediction intervals for BART-BMA estimates by post-hoc Gibbs-sampling from the full conditionals of the terminal node parameters and the variance of the error term. See Hernandez et al. (2018) Appendix D for details.
+#' @description This function produces predictions from BART-BMA by post-hoc Gibbs-sampling from the full conditionals of the terminal node parameters and the variance of the error term. See Hernandez et al. (2018) Appendix D for details.
 #' @param object bartBMA object obtained from function bartBMA
 #' @param num_iter Total number of iterations of the Gibbs sampler (including burn-in).
 #' @param burnin Number of burn-on iterations of the Gibbs sampler.
-#' @param l_quant Lower quartile of the prediction interval.
-#' @param u_quant Upper quartile of the prediction interval.
 #' @param newdata Test data for which predictions are to be prodiced. Default = NULL. If NULL, then produces prediction intervals for training data if no test data was used in producing the bartBMA object, or produces prediction intervals for the original test data if test data was used in producing the bartBMA object.
 #' @export 
 #' @return The output is a list of length one. The one element in this list is a vector of prediction intervals???
 
-pred_intervals<-function(object,num_iter,burnin,l_quant,u_quant,newdata=NULL){
-  if(l_quant>0.5 ||u_quant<0 ||u_quant>1){stop("Lower quantile must be lower than 0.5 and greater than 0")}
-  if(u_quant<0.5 ||u_quant<0 ||u_quant>1){stop("Upper quantile must be greater than 0.5 and less than 1")}
+pred_means_bbma<-function(object,num_iter,burnin,newdata=NULL){
   #object will be bartBMA object.
   if(is.null(newdata) && length(object)==16){
     #if test data specified separately
@@ -32,21 +28,21 @@ pred_intervals<-function(object,num_iter,burnin,l_quant,u_quant,newdata=NULL){
   y_posterior_sum_trees<-gs_chains[[4]]
   y_orig_post_sum_trees<-gs_chains[[5]]
   sigma_chains<-gs_chains[[3]]
-#  if(is.null(newdata) && length(object)==16){
-#    y_posterior_sum_trees<-gs_chains[[4]]
-#    y_orig_post_sum_trees<-gs_chains[[5]]
-#    sigma_chains<-gs_chains[[3]]
-#  }else if(is.null(newdata) && length(object)==14){
-#    y_posterior_sum_trees<-gs_chains[[4]]
-#    y_orig_post_sum_trees<-gs_chains[[5]]
-#    sigma_chains<-gs_chains[[3]]
-#
-#  }else{
-#    y_posterior_sum_trees<-gs_chains[[4]]
-#    y_orig_post_sum_trees<-gs_chains[[5]]
-#    sigma_chains<-gs_chains[[3]]
-#  } 
-
+  #  if(is.null(newdata) && length(object)==16){
+  #    y_posterior_sum_trees<-gs_chains[[4]]
+  #    y_orig_post_sum_trees<-gs_chains[[5]]
+  #    sigma_chains<-gs_chains[[3]]
+  #  }else if(is.null(newdata) && length(object)==14){
+  #    y_posterior_sum_trees<-gs_chains[[4]]
+  #    y_orig_post_sum_trees<-gs_chains[[5]]
+  #    sigma_chains<-gs_chains[[3]]
+  #
+  #  }else{
+  #    y_posterior_sum_trees<-gs_chains[[4]]
+  #    y_orig_post_sum_trees<-gs_chains[[5]]
+  #    sigma_chains<-gs_chains[[3]]
+  #  } 
+  
   sum_of_tree_BIC<- object$bic
   weights<-exp(sum_of_tree_BIC-(max(sum_of_tree_BIC)+log(sum(exp(sum_of_tree_BIC-max(sum_of_tree_BIC))))))
   #final_length<-num_iter-burnin
@@ -66,15 +62,15 @@ pred_intervals<-function(object,num_iter,burnin,l_quant,u_quant,newdata=NULL){
     final_yorig_chain<-rbind(final_yorig_chain,post_yorig_i[sample_its,])
     
   }
-  PI<-apply(final_yorig_chain,2,function(x)quantile(x,probs=c(l_quant,0.5,u_quant)))
   meanpreds<-apply(final_yorig_chain,2,mean)
   
   
-  ret<-list()
-  length(ret)<-1
-  ret[[1]]<-PI
-  ret[[2]] <- meanpreds
-
-  class(ret)<-"pred_intervals.bartBMA"  
+  ret<-meanpreds
+  #length(ret)<-1
+  #ret[[1]]<-meanpreds
+  #ret[[2]] <- final_y_chain
+  #ret[[3]] <- post_y_i
+  
+  class(ret)<-"pred_means_bbma.bartBMA"  
   ret
 }
